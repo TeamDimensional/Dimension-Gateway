af3ed317d7f0e9c1955dedbd096e0e5e96786cb6
Wizzerinus <development@toontowneventhorizon.com>
2024-06-27 20:11:22 +0300

    attempt to make a github workflow

---
+++ compile.py
@@ -0,0 +1,68 @@
+import dataclasses
+import tomllib
+import os
+import json
+import shutil
+
+
+@dataclasses.dataclass
+class File:
+    projectID: int
+    fileID: int
+    required: bool = True
+
+
+@dataclasses.dataclass
+class ModLoader:
+    id: str
+    primary: bool = True
+
+
+@dataclasses.dataclass
+class Minecraft:
+    version: str
+    modLoaders: list[ModLoader]
+
+
+@dataclasses.dataclass
+class Manifest:
+    minecraft: Minecraft
+    name: str
+    version: str
+    author: str
+    files: list[File]
+    manifestType: str = "minecraftModpack"
+    manifestVersion: int = 1
+    overrides: str = "overrides"
+
+
+def make_manifest(pack: dict, files: list[dict]):
+    mc = Minecraft(version=pack["version"], modLoaders=[ModLoader(pack["versions"]["forge"])])
+    filesConverted = [File(f["update"]["curseforge"]["project-id"], f["update"]["curseforge"]["file-id"]) for f in files]
+    return Manifest(mc, pack["name"], pack["version"], pack["author"], filesConverted)
+
+
+def run():
+    with open("metadata/pack.toml", "rb") as f:
+        data = tomllib.load(f)
+    files = []
+    for f in os.listdir("metadata/mods"):
+        with open("metadata/mods/" + f, "rb") as fp:
+            files.append(tomllib.load(fp))
+    manifest = make_manifest(data, files)
+
+    shutil.rmtree("build", ignore_errors=True)
+    os.makedirs("build/overrides/mods", exist_ok=True)
+    with open("build/manifest.json", "w") as f:
+        json.dump(dataclasses.asdict(manifest), f)
+    shutil.copytree("config", "build/overrides/config")
+    shutil.copytree("groovy", "build/overrides/groovy")
+    for f in os.listdir("mods"):
+        if f.startswith("gatewaycore"):
+            shutil.copy("mods/" + f, "build/overrides/mods/" + f)
+    shutil.make_archive("dimension-gateway-" + data["version"], "zip", "build", ".")
+    shutil.move("dimension-gateway-" + data["version"] + ".zip", "build")
+
+
+if __name__ == "__main__":
+    run()
