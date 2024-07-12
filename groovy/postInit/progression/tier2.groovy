dfd8c412f92bbcc58259e8e6367fca648bea654b
Wizzerinus <development@toontowneventhorizon.com>
2024-07-12 15:49:58 +0300

    remove all cringe

--- groovy/postInit/progression/tier2.groovy
+++ groovy/postInit/progression/tier2.groovy
@@ -56,7 +56,7 @@ inWorldCrafting.pistonPush.recipeBuilder()
     .input(item("naturesaura:gold_leaf"))
     .output(item("naturesaura:gold_powder"))
     .minHarvestLevel(1)
-    .maxConversionsPerPush(2)
+    .maxConversionsPerPush(16)
     .register()
 
 // Incinerator
