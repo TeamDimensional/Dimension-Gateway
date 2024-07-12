3f5a7594d32a29466223e76e1454fb21e8e379d5
Wizzerinus <development@toontowneventhorizon.com>
2024-07-12 15:40:44 +0300

    fixed wilted daisy sounds

--- groovy/preInit/blocks.groovy
+++ groovy/preInit/blocks.groovy
@@ -1,7 +1,11 @@
 import net.minecraft.block.BlockBush
+import net.minecraft.block.SoundType
 
 class BlockWiltedDaisy extends BlockBush {
-    public BlockWiltedDaisy() {super()}
+    public BlockWiltedDaisy() {
+        super()
+        setSoundType(SoundType.PLANT)
+    }
 }
 
 content.registerBlock("wilted_daisy", new BlockWiltedDaisy())
