622b71ee4431bbd4d3de7128b8ae9395edd1b674
Wizzerinus <quillinde@gmail.com>
2026-02-28 21:13:05 +0300

    ban tool harvest levels

--- minecraft/groovy/classes/GatewayHelpers.groovy
+++ minecraft/groovy/classes/GatewayHelpers.groovy
@@ -4,6 +4,8 @@ import net.minecraft.util.text.TextFormatting
 import net.darkhax.gamestages.GameStageHelper
 import net.minecraft.entity.player.EntityPlayer
 import com.dimensional.gatewaycore.events.TooltipEvents
+import com.dimensional.gatewaycore.events.ToolUtils
+import net.minecraft.item.ItemTool
 
 class GatewayHelpers {
     static def colors = ["white", "orange", "magenta", "light_blue", "yellow", "lime", "pink", "gray",
@@ -17,7 +19,10 @@ class GatewayHelpers {
 
     static void banTool(ItemStack itemStack) {
         itemStack.getItem().setMaxDamage(1)
-        TooltipEvents.setTooltip(itemStack, "This item is disabled! Use Tinker's Construct tools instead.", textformat("RED"))
+        itemStack.getItem().setHarvestLevel("pickaxe", -1)
+        if (itemStack.getItem() instanceof ItemTool)
+            ToolUtils.deleteToolType(itemStack)
+        TooltipEvents.setTooltip(itemStack, "This item is disabled! Use Tinker's Construct tools or other materials instead.", textformat("RED"))
     }
 
     static void run() {}
