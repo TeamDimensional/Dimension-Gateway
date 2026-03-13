ab6a61f09ae3e65e8280088a8be7132e045722cc
Wizzerinus <quillinde@gmail.com>
2026-03-14 00:17:06 +0300

    replace drawers with functional storage, add sophisticated backpacks

---
+++ minecraft/groovy/postInit/cleanup_tiers/storage_mods.groovy
@@ -0,0 +1,56 @@
+import classes.GatewayHelpers
+import com.dimensional.gatewaycore.events.TooltipEvents
+
+def mitem(name, k=0) {
+    return item("functionalstoragelegacy:${name}", k)
+}
+def bitem(name, k=0) {
+    return item("retro_sophisticated_backpacks:${name}", k)
+}
+
+def tier2Items = [
+    mitem("copper_upgrade"), mitem("compacting_drawer"), mitem("simple_compacting_drawer"), bitem("stack_upgrade_starter_tier"),
+]
+
+def tier3Items = [
+    mitem("gold_upgrade"), mitem("redstone_upgrade"), mitem("fluid_1"), mitem("fluid_2"), mitem("fluid_4"),
+    bitem("backpack_iron"), bitem("pickup_upgrade"), bitem("filter_upgrade"),
+]
+
+def tier4Items = [
+    mitem("diamond_upgrade"), mitem("void_upgrade"), mitem("collector_upgrade"), mitem("linking_tool"),
+    mitem("storage_controller"), mitem("controller_extension"), bitem("stack_upgrade_tier_1"),
+]
+
+def tier5Items = [
+    mitem("netherite_upgrade"), bitem("backpack_gold"), bitem("advanced_pickup_upgrade"), bitem("feeding_upgrade"),
+    bitem("advanced_deposit_upgrade"), bitem("advanced_restock_upgrade"), bitem("advanced_filter_upgrade"),
+]
+
+def tier6Items = [
+    bitem("backpack_diamond"), bitem("stack_upgrade_tier_2"), bitem("advanced_feeding_upgrade"),
+]
+
+def tier7Items = [
+    mitem("ender_drawer"), mitem("armory_cabinet"), bitem("stack_upgrade_tier_3"),
+]
+
+def tier8Items = [
+    bitem("backpack_obsidian"),
+]
+
+def tier10Items = [
+    bitem("stack_upgrade_tier_4"), bitem("exponential_stack_upgrade"), bitem("inception_upgrade"),
+]
+
+def creativeItems = [mitem("creative_vending_upgrade")]
+
+for (def it in tier2Items) TooltipEvents.setTier(it, 2)
+for (def it in tier3Items) TooltipEvents.setTier(it, 3)
+for (def it in tier4Items) TooltipEvents.setTier(it, 4)
+for (def it in tier5Items) TooltipEvents.setTier(it, 5)
+for (def it in tier6Items) TooltipEvents.setTier(it, 6)
+for (def it in tier7Items) TooltipEvents.setTier(it, 7)
+for (def it in tier8Items) TooltipEvents.setTier(it, 8)
+for (def it in tier10Items) TooltipEvents.setTier(it, 10)
+for (def it in creativeItems) TooltipEvents.setTier(it, 15)
