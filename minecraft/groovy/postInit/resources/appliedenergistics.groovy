ba26232d9ec9034a4dd11f24c6cdfa39fd239a8a
Wizzerinus <quillinde@gmail.com>
2026-08-28 21:47:59 +0300

    use Samarium for 16m compacting cells

--- minecraft/groovy/postInit/resources/appliedenergistics.groovy
+++ minecraft/groovy/postInit/resources/appliedenergistics.groovy
@@ -163,7 +163,7 @@ def components = [
     [item("nae2:material", 7), item("nae2:material", 8), item("appliedenergistics2:material", 24),
      item("nuclearcraft:alloy", 16), item("essentialcraft:genitem", 32), "16384k_fluid"],
     [item("cells:compacting_component", 6), item("cells:compacting_component", 7), item("cells:overclocked_processor", 1),
-     item("nuclearcraft:ingot2", 7), item("essentialcraft:genitem", 32), "16384k_compacting"],
+     item("nuclearcraft:alloy", 18), item("essentialcraft:genitem", 32), "16384k_compacting"],
 ]
 
 for (def xs in components) {
